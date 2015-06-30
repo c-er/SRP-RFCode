868c5246027f169395dba98ff9ef844e29ee2130
Siddharth Kurella <mastersidkurella@gmail.com>
2015-06-30 10:11:55 -0400

    Created Tag and Reader classes

---
+++ src/rfid/rfcode/Reader.java
@@ -0,0 +1,12 @@
+package rfid.rfcode;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sid on 6/30/15.
+ */
+public class Reader {
+    private String label;
+    private String connectionState;
+    private ArrayList<Tag> tagsConnected;
+}
---
+++ src/rfid/rfcode/Tag.java
@@ -0,0 +1,11 @@
+package rfid.rfcode;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sid on 6/30/15.
+ */
+public class Tag {
+    private String ID;
+    private String group;
+}
