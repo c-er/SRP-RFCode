3d49f2e5887249b39e3b0c8785d5e7165bd9db5a
blaziken311 <arjun.stv@gmail.com>
2015-06-29 20:33:58 -0400

    Fixed your exception stuff
    If I messed something up it's because I learned this just now.

--- src/SocketTest.java
+++ src/SocketTest.java
@@ -8,14 +8,21 @@ import java.net.Socket;
  * Created by uday on 6/25/15.
  */
 public class SocketTest {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        
         Socket sock = new Socket("10.11.34.186", 6501);
         PrintWriter out = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-        in.readLine();
-        in.read();
-        in.read();
-        out.println("tagcount.xml\r");
+        
+        try {
+            in.readLine();
+            in.read();
+            in.read();
+            out.println("tagcount.xml\r");
+        } catch ( IOException ioe ) {
+            System.out.println( "IOException: " + ioe.getMessage() + "@ Line 17" );
+        }
+        
         String str = "";
         while((str = in.readLine()) != null)
         {
